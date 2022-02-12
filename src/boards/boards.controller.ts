4cf11c0467a5837d3cc94c6ca7864f02960ca9c9
rugaheedori <qufslarkgml@gmail.com>
2022-02-12 13:18:55 +0900

    boards Controller로 들어온 GET요청 Service에서 handle후 Controller로 리턴틀 제작

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,9 +1,12 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
 export class BoardsController {
-  constructor(private boardsService: BoardsService) {
-    this.boardsService;
+  constructor(private boardsService: BoardsService) {}
+
+  @Get('/')
+  getAllBoard() {
+    this.boardsService.getAllBoards();
   }
 }
