bb8941ae6f8d65951c04340e81396eb0151a1d76
rugaheedori <qufslarkgml@gmail.com>
2022-02-13 22:43:59 +0900

    Create Board Model, getAllBoard() 타입 지정

--- src/boards/boards.controller.ts
+++ src/boards/boards.controller.ts
@@ -1,4 +1,5 @@
 import { Controller, Get } from '@nestjs/common';
+import { Board } from './board.model';
 import { BoardsService } from './boards.service';
 
 @Controller('boards')
@@ -6,7 +7,7 @@ export class BoardsController {
   constructor(private boardsService: BoardsService) {}
 
   @Get('/')
-  getAllBoard() {
-    this.boardsService.getAllBoards();
+  getAllBoard(): Board[] {
+    return this.boardsService.getAllBoards();
   }
 }
