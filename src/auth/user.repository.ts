e6a09df4d94a523b521d65146883ec4aa77704cc
rugaheedori <qufslarkgml@gmail.com>
2022-03-02 14:41:27 +0900

    bcrypt를 이용하여 비밀번호 암호화 하기

--- src/auth/user.repository.ts
+++ src/auth/user.repository.ts
@@ -5,12 +5,17 @@ import {
 import { EntityRepository, Repository } from 'typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { User } from './user.entity';
+import * as bcrypt from 'bcryptjs';
 
 @EntityRepository(User)
 export class UserRepository extends Repository<User> {
   async createUser(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     const { userName, password } = authCredentialsDto;
-    const user = this.create({ userName, password });
+
+    const salt = await bcrypt.genSalt();
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = this.create({ userName, password: hashedPassword });
 
     try {
       await this.save(user);
