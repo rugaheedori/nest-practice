051149b6ba5cbe31b1a8b8758fa5fe6f5a6fd726
rugaheedori <qufslarkgml@gmail.com>
2022-03-02 14:51:26 +0900

    로그인 기능 구현

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -12,4 +12,11 @@ export class AuthController {
   ): Promise<void> {
     return this.authService.signUp(authCredentialsDto);
   }
+
+  @Post('/sign-in')
+  signIn(
+    @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
+  ): Promise<string> {
+    return this.authService.signIn(authCredentialsDto);
+  }
 }
