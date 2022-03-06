a45ef595bfe093365254d93d579d25a9c811011a
rugaheedori <qufslarkgml@gmail.com>
2022-03-06 22:14:17 +0900

    jwt를 이용하여 사용자 accessToken 생성하기

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,7 +16,7 @@ export class AuthController {
   @Post('/sign-in')
   signIn(
     @Body(ValidationPipe) authCredentialsDto: AuthCredentialsDto,
-  ): Promise<string> {
+  ): Promise<{ accessToken: string }> {
     return this.authService.signIn(authCredentialsDto);
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -3,24 +3,32 @@ import { InjectRepository } from '@nestjs/typeorm';
 import { AuthCredentialsDto } from './dto/auth-credential.dto';
 import { UserRepository } from './user.repository';
 import * as bcrypt from 'bcryptjs';
+import { JwtService } from '@nestjs/jwt';
 
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserRepository)
     private userRepository: UserRepository,
+    private jwtService: JwtService,
   ) {}
 
   async signUp(authCredentialsDto: AuthCredentialsDto): Promise<void> {
     return this.userRepository.createUser(authCredentialsDto);
   }
 
-  async signIn(authCredentialsDto: AuthCredentialsDto): Promise<string> {
+  async signIn(
+    authCredentialsDto: AuthCredentialsDto,
+  ): Promise<{ accessToken: string }> {
     const { userName, password } = authCredentialsDto;
     const user = await this.userRepository.findOne({ userName });
 
     if (user && (await bcrypt.compare(password, user.password))) {
-      return 'login success';
+      // 유저 토큰 생성( Secret + payload )
+      const payload = { userName };
+      const accessToken = this.jwtService.sign(payload);
+
+      return { accessToken };
     }
     throw new UnauthorizedException('login failed');
   }
