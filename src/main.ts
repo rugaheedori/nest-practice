292918eb17bbd2fb4836466c77f82f8fcdac5946
rugaheedori <qufslarkgml@gmail.com>
2022-04-13 23:39:36 +0900

    config 파일 추가 => 공통, develop, product

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,9 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const port = 3000;
+
   await app.listen(port);
+
   Logger.log(`Application running on port ${port}`);
 }
 bootstrap();
